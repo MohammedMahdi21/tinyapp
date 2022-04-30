2c03f567a034fe4c7a3771a9edeb15e4964195dd
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-30 04:30:43 +0000

    add a new route handler for /urls and use res.render() to pass the URL data to the template.

--- express_server.js
+++ express_server.js
@@ -10,9 +10,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (req,res) => {
   res.send("Hello!");
 });
+
+app.get("/urls", (req,res) =>{
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
 
 app.get("/url.json", (req, res) =>{
   res.json(urlDatabase);
